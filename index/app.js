9b14f0785ab01c9e506e80db80b8165c067b0c8a
HSKGecko <support@hskgecko.com>
2023-01-19 21:24:01 +0000

    path fix

--- index/app.js
+++ index/app.js
@@ -3,12 +3,14 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
+app.use(express.static(__dirname + '/public'));
+
 router.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
     //__dirname : It will resolve to your project folder.
 });
 
-router.get('/Neil', function(req, res) {
+router.get('/neil', function(req, res) {
     res.sendFile(path.join(__dirname + '/neil.html'));
 })
 // router.get('/about', function (req, res) {
@@ -23,4 +25,4 @@ router.get('/Neil', function(req, res) {
 app.use('/', router);
 app.listen(process.env.port || 80);
 
-console.log('Running at Port 3000');
+console.log('Running at Port 80');
